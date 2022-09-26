0d672db8a06bf635493d2a01b0dc32746c93b0d1
Ward Beyens <wardbeyens99@gmail.com>
2022-09-26 10:12:23 +0200

    Fix compatibility

--- src/index.js
+++ src/index.js
@@ -37,7 +37,7 @@ const cache = apicache.options({
 
 api.get('/stations', cache('24 hours'), stationsByQuery)
 api.get('/stations/:id', cache('24 hours'), stationById)
-api.get('/id/:id', cache('24 hours'), reachableFrom)
+api.get('/:id', cache('24 hours'), reachableFrom)
 
 api.disable('x-powered-by')
 
