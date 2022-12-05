194590193f93e55ef0d5d8f0b9453bf351dc377f
Ward Beyens <wardbeyens99@gmail.com>
2022-12-05 11:06:54 +0100

    Update build stations

--- src/build-stations.js
+++ src/build-stations.js
@@ -3,33 +3,48 @@ import countriesList from 'countries-list'
 import fetchTrainlineStations, { collect } from 'trainline-stations'
 
 const buildStations = async () => {
-	console.error('fetching latest stations…')
+	console.error('Fetching latest stations…')
 	const rawStations = await collect(fetchTrainlineStations())
 
-	console.error('building station dataset…')
-	const validAndFormattedStations = rawStations
-		.flatMap(s => {
-			if (!(s.id && s.db_id && s.name && s.is_suggestable && countriesList.countries[s.country] && s.longitude && +s.longitude < 180 && s.latitude && +s.latitude < 90)) return []
-			const id = String(s.db_id)
-			return [{
-				id: (id.length === 9 && id.slice(0, 2)) ? id.slice(2) : id,
+	console.error('Building suggestable stations dataset…')
+	const validAndFormattedStations = rawStations.flatMap((s) => {
+		if (
+			!(
+				s.id &&
+				s.db_id &&
+				s.name &&
+				s.is_suggestable &&
+				s.longitude &&
+				+s.longitude < 180 &&
+				s.latitude &&
+				+s.latitude < 90 &&
+				countriesList.countries[s.country]
+			)
+		) {
+			return []
+		}
+		const id = String(s.db_id)
+		return [
+			{
+				id: id.length === 9 && id.slice(0, 2) ? id.slice(2) : id,
 				name: s.name,
-				isMeta: s.is_city,
 				country: s.country,
+				slug: s.slug,
 				location: {
 					longitude: +s.longitude,
 					latitude: +s.latitude,
 				},
-			}]
-		})
+			},
+		]
+	})
 
-	console.error('done.')
-	return fromPairs(validAndFormattedStations.map(s => [s.id, s]))
+	console.error('Done.')
+	return fromPairs(validAndFormattedStations.map((s) => [s.id, s]))
 }
 
 buildStations()
-	.then(stations => process.stdout.write(JSON.stringify(stations, null, 2)))
-	.catch(error => {
+	.then((stations) => process.stdout.write(JSON.stringify(stations, null, 2)))
+	.catch((error) => {
 		console.error(error)
 		process.exit(1)
 	})
